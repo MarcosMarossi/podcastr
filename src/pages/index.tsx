91c4c7b361b5ea97d491790791107253a1359662
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2021-05-16 15:05:42 -0300

    Conversão de dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,61 @@
-export default function Home() {
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/dateUtil';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  publishedAt: string;
+  thumbnail: string;
+  description: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+}
+
+type HomeProps = {
+  episodes: Episode[],
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <></>
+    <div>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc,'
+    }
+  });
+
+  const episodes = data.map((episode: any) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  });
+ 
+  return {
+    props: {
+      episodes,
+    },
+  }
 }
